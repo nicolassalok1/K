7319213b47892350f86477db6feb15eec9cb1ad7
Nicolas Pelletier <nicolas93pelletier@gmail.com>
2018-01-23 18:14:43 -0800

    {ADD} CAPS LOCK

--- k/keyboard.c
+++ k/keyboard.c
@@ -8,23 +8,24 @@
 #include "monitor.h"
 
 static int col = 0;
+static int caps = 0;
 
 const unsigned char kbdus[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
   '9', '0', '-', '=', '\b',	/* Backspace */
   '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
+  'a', 'z', 'e', 'r',	/* 19 */
   't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+    'à',			/* 29   - Control */
+  'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
  '\'', '`',   0,		/* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+ '\\', 'w', 'x', 'c', 'v', 'b', 'n',			/* 49 */
   'm', ',', '.', '/',   0,				/* Right shift */
   '*',
-    0,	/* Alt */
+    'ç',	/* Alt */
   ' ',	/* Space bar */
-    0,	/* Caps lock */
+    '&',	/* Caps lock */
     0,	/* 59 - F1 key ... > */
     0,   0,   0,   0,   0,   0,   0,   0,
     0,	/* < ... F10 */
@@ -49,6 +50,16 @@ const unsigned char kbdus[128] =
     0,	/* All other keys are undefined */
 };
 
+const unsigned char CAPS[128] =
+{
+  'A', 'Z', 'E', 'R',	/* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\n',	/* Enter key */
+    'à',			/* 29   - Control */
+  'Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'W', 'X', 'C', 'V', 'B', 'N'			/* 49 */
+};
+
 /* Handles the keyboard interrupt */
 void keyboard_handler()
 {
@@ -60,27 +71,41 @@ void keyboard_handler()
     }
     else
     {
-        col++;
-        monitor_put(kbdus[scancode]);
-        if (col == 79 )
+
+        if (kbdus[scancode] == '&')
         {
-          monitor_put('\n');
-          col = 0;
+          caps = caps == 1 ? 0 : 1;
         }
-
-        if (kbdus[scancode] == '\n')
+        else
         {
-          col = 0;
+          col++;
+          if (caps == 0)
+          {
+            monitor_put(kbdus[scancode]);
+          }
+          else
+          {
+            monitor_put(CAPS[scancode - 16]);
+          }
+
+           if (col == 79)
+          {
+            monitor_put('\n');
+            col = 0;
+          }
+
+          if (kbdus[scancode] == '\n')
+          {
+            col = 0;
+          }
+
+          if (kbdus[scancode] == '\b')
+          {
+            monitor_write(" \b");
+            col--;
+            col--;
+          }
         }
-
-        if (kbdus[scancode] == '\b')
-        {
-          monitor_write(" \b");
-          col--;
-          col--;
-        }
-
-
     }
 }
 
